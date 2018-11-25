b6d51d0345abf0024e6e3a3587416f99e7834b5e
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-11-25 11:24:43 +0100

    allow configurable session cache endpoint

--- session.go
+++ session.go
@@ -4,9 +4,17 @@ import (
 	"github.com/astaxie/beego/session"
 	// Dialect import, not used directly
 	_ "github.com/astaxie/beego/session/memcache"
+    "os"
 )
 
 var globalSessions *session.Manager
+
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
 
 func init() {
 	sessionConfig := &session.ManagerConfig{
@@ -14,7 +22,7 @@ func init() {
 		Gclifetime:      3600,
 		EnableSetCookie: true,
 		Secure:          false,
-		ProviderConfig:  "127.0.0.1:11211",
+		ProviderConfig:  getEnv("MEMCACHED_SESSION_URL", "127.0.0.1:11211"),
 	}
 	globalSessions, _ = session.NewManager("memcache", sessionConfig)
 	go globalSessions.GC()
