e330fc2bb125a1b1db95e810ab249c9a195eed86
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-12-04 23:56:54 +0100

    render user identification on homepage

--- home.go
+++ home.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/jonlil/nibestats/models"
 	"net/http"
 )
 
@@ -9,6 +10,25 @@ func (s *Server) HandleHome() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		sess, _ := globalSessions.SessionStart(w, r)
 		defer sess.SessionRelease(w)
-		rnd.HTML(w, http.StatusOK, "home", nil)
+		var user *models.User = nil
+		var accessToken *models.AccessToken = nil
+
+		if sess.Get("UserID") != nil {
+			user = &models.User{}
+			accessToken = &models.AccessToken{}
+
+			if s.DB.First(&user, sess.Get("UserID")).RecordNotFound() {
+				user = nil
+			}
+
+			if s.DB.Where("user_id = ? AND user_id IS NOT NULL", user.ID).First(&accessToken).RecordNotFound() {
+				accessToken = nil
+			}
+		}
+
+		rnd.HTML(w, http.StatusOK, "home", map[string]interface{}{
+			"user":            user,
+			"nibe_connection": accessToken,
+		})
 	}
 }
